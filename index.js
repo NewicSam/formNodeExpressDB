d8d2a0bbc40d6fc3fba5b27836a55854ba9f4fc9
samuel <72706296+samueletienneanneghislain@users.noreply.github.com>
2022-05-11 11:08:06 +0200

    port changes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const Datastore = require('nedb');
 
 const app = express();
 const database = new Datastore('database.db');
-const port = 4000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Starting server at ${ port }`));
 app.use(express.static('public'));
